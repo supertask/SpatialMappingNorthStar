e992d1e1bc351dc09285e1d4f9b166e58218cbb8
Tasuku <lightfox.task@gmail.com>
2019-04-22 04:44:19 +0900

    Init

---
+++ Assets/SpatialMappingNorthStar/phoenix/Curl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public static class Curl
+{
+    private static float snoise(Vector3 v)
+    {
+        return SimplexNoise.Noise.Generate(v.x, v.y, v.z);
+    }
+
+    public static Vector3 snoiseVec3(Vector3 x)
+    {
+        float s = snoise(x);
+        float s1 = snoise(new Vector3(x.y - 19.1f, x.z + 33.4f, x.x + 47.2f));
+        float s2 = snoise(new Vector3(x.z + 74.2f, x.x - 124.5f, x.y + 99.4f));
+        return new Vector3(s, s1, s2);
+    }
+
+    //Simplex Curl Noise
+    public static Vector3 Noise(Vector3 p)
+    {
+        const float e = 0.001f;
+        Vector3 dx = new Vector3(e, 0.0f, 0.0f);
+        Vector3 dy = new Vector3(0.0f, e, 0.0f);
+        Vector3 dz = new Vector3(0.0f, 0.0f, e);
+
+        Vector3 p_x0 = snoiseVec3(p - dx);
+        Vector3 p_x1 = snoiseVec3(p + dx);
+        Vector3 p_y0 = snoiseVec3(p - dy);
+        Vector3 p_y1 = snoiseVec3(p + dy);
+        Vector3 p_z0 = snoiseVec3(p - dz);
+        Vector3 p_z1 = snoiseVec3(p + dz);
+
+        float x = p_y1.z - p_y0.z - p_z1.y + p_z0.y;
+        float y = p_z1.x - p_z0.x - p_x1.z + p_x0.z;
+        float z = p_x1.y - p_x0.y - p_y1.x + p_y0.x;
+
+        const float divisor = 1.0f / (2.0f * e);
+        return Vector3.Normalize(new Vector3(x, y, z) * divisor);
+    }
+}
---
+++ Assets/SpatialMappingNorthStar/phoenix/PhoenixController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Klak.Math;
+
+public class PhoenixController : MonoBehaviour
+{
+    public float timeScale = 0.1f;
+    public float positionScale = 1.0f;
+    public float _noiseFrequency = 0.3f;
+    public int _randomSeed = 123;
+    public float _stepFrequency = 2.0f;
+
+    Animator anim;
+    NoiseGenerator _noise;
+    XXHash _hash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.anim = this.GetComponent<Animator>();
+        this.anim.speed = 0.8f;
+        this.anim.Play("Take 001");
+        this._hash = new XXHash(_randomSeed);
+        this._noise = new NoiseGenerator(_randomSeed, _noiseFrequency);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._noise.Frequency = _noiseFrequency;
+        this._noise.Step();
+        var delta = this._stepFrequency * Time.deltaTime;
+
+        RaycastHit hit;
+        Vector3 obstacle_dir = Vector3.zero;
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, 3) ) {
+            if (hit.transform != transform) {
+                Debug.DrawLine(this.transform.position, hit.point, Color.red);
+                obstacle_dir = hit.normal * 5;
+            }
+
+        }
+
+        //Debug.Log("new pos: " + newPos);
+        this.transform.rotation = this._noise.Rotation(0, 90f, 360f, 90f) * Quaternion.LookRotation(obstacle_dir);
+        this.transform.position += 2 * this.transform.forward* delta;
+
+        //Debug.Log("rot: " + this.transform.rotation.eulerAngles);
+    }
+
+}

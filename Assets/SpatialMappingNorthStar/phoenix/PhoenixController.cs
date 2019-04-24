456f808fc731fff9793995f8ff43ad33e8e96661
Tasuku Takahahashi <lightfox.task@gmail.com>
2019-04-25 07:12:44 +0900

    Added obstacle avoidance.

--- Assets/SpatialMappingNorthStar/phoenix/PhoenixController.cs
+++ Assets/SpatialMappingNorthStar/phoenix/PhoenixController.cs
@@ -30,23 +30,50 @@ public class PhoenixController : MonoBehaviour
     {
         this._noise.Frequency = _noiseFrequency;
         this._noise.Step();
-        var delta = this._stepFrequency * Time.deltaTime;
+        float delta = this._stepFrequency * Time.deltaTime;
+		Quaternion current_rot = this.transform.rotation;
 
-        RaycastHit hit;
-        Vector3 obstacle_dir = Vector3.zero;
-        if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, 3) ) {
-            if (hit.transform != transform) {
-                Debug.DrawLine(this.transform.position, hit.point, Color.red);
-                obstacle_dir = hit.normal * 5;
-            }
+		Vector3 obstacle_dir = this.GetObstacleAvoidedDir();
 
-        }
+		//Debug.Log("new pos: " + newPos);
 
-        //Debug.Log("new pos: " + newPos);
-        this.transform.rotation = this._noise.Rotation(0, 90f, 360f, 90f) * Quaternion.LookRotation(obstacle_dir);
-        this.transform.position += 2 * this.transform.forward* delta;
+		Quaternion target_rot = this._noise.Rotation(0, 90f, 360f, 90f) * Quaternion.LookRotation(obstacle_dir);
+		this.transform.rotation = Quaternion.Slerp(current_rot, target_rot, delta);
+        this.transform.position += 2 * this.transform.forward * delta;
 
         //Debug.Log("rot: " + this.transform.rotation.eulerAngles);
     }
+
+	private Vector3 GetObstacleAvoidedDir() {
+		Vector3 obstacle_dir = Vector3.zero;
+		RaycastHit hit;
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, 3)) {
+            if (hit.transform != transform) {
+                Debug.DrawLine(this.transform.position, hit.point, Color.red);
+                obstacle_dir = hit.normal * 50;
+            }
+        }
+
+		Vector3 leftR = this.transform.position;
+        Vector3 rightR = this.transform.position;
+        leftR.x -= 2;
+        rightR.x += 2;
+        //Left
+        if (Physics.Raycast(leftR, this.transform.forward, out hit, 20)) {
+            if (hit.transform != transform) {
+                Debug.DrawLine(leftR, hit.point, Color.red);
+				obstacle_dir += hit.normal * 50;
+            }
+        }
+        //Right
+        if (Physics.Raycast(rightR, this.transform.forward, out hit, 20)) {
+            if (hit.transform != transform) {
+                Debug.DrawLine(rightR, hit.point, Color.red);
+				obstacle_dir += hit.normal * 50;
+            }
+        }
+
+		return obstacle_dir;
+	}
 
 }
